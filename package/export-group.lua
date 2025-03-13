18988eb9225049f2d62693348989e7bb3015d95d
aleokdev <aleok.inf@gmail.com>
2025-03-13 18:06:09 +0100

    Misc improvements & refactors on code

--- package/export-group.lua
+++ package/export-group.lua
@@ -1,3 +1,15 @@
+-- This script exports each layer in the current group as a separate file.
+-- The user can specify the directory, filename format, and export format.
+-- The filename format can contain the following placeholders:
+--   {groupname} - the name of the group
+--   {layername} - the name of the layer
+-- The export format can be any of the following (Supported aseprite formats from v1.3.13):
+--   ase, aseprite, bmp, css, flc, fli, gif, ico, jpeg, jpg, pcx, pcc, png, qoi, svg, tga, webp
+-- The user can also specify whether to trim each layer before exporting.
+
+EXPORT_FORMATS = { 'ase', 'aseprite', 'bmp', 'css', 'flc', 'fli', 'gif', 'ico', 'jpeg', 'jpg', 'pcx', 'pcc', 'png', 'qoi',
+  'svg', 'tga', 'webp' }
+
 function init(plugin)
   plugin:newCommand {
     id = "ExportGroupAs",
@@ -6,38 +18,37 @@ function init(plugin)
     onclick = function()
       local dlg = Dialog {
         title = "Export Group As",
-        hexpand = true
       }
       dlg:newrow { always = true }
-      just_modified = false
+
+      CHANGED_TO_PARENT_DIR = false
       dlg:file {
-        id = "file",
+        id = "output_dir_path",
         label = "Directory to export to",
         save = true,
         filetypes = { '' },
         filename = plugin.preferences.export_dir or "Select directory",
         onchange = function()
-          if just_modified then return end
+          if CHANGED_TO_PARENT_DIR then return end
 
           -- Use the directory as the "filename" to show
-          local filename = dlg.data.file
+          local filename = dlg.data.output_dir_path
           filename = filename:gsub("\\", "/")
           local parent_dir = filename:match("^(.+)/[^/]*$")
           if parent_dir then
-            print("parent dir " .. parent_dir)
             -- Prevent infinite loop (onchange is called when we modify the filename)
-            just_modified = true
+            CHANGED_TO_PARENT_DIR = true
             dlg:modify { id = "file", filename = parent_dir }
-            just_modified = false
+            CHANGED_TO_PARENT_DIR = false
           end
         end
       }
       dlg:entry { id = "filename_format", label = "Filename format", text = plugin.preferences.export_filename_format or "{groupname}-{layername}" }
       dlg:combobox {
-        id = 'format',
+        id = 'export_format',
         label = 'Export format',
         option = plugin.preferences.export_format or 'png',
-        options = { 'ase', 'aseprite', 'bmp', 'css', 'flc', 'fli', 'gif', 'ico', 'jpeg', 'jpg', 'pcx', 'pcc', 'png', 'qoi', 'svg', 'tga', 'webp' }
+        options = EXPORT_FORMATS
       }
       dlg:check {
         id = "trim",
@@ -49,11 +60,11 @@ function init(plugin)
 
       if not dlg.data.ok then return 0 end
 
-      local extension = dlg.data.format
-      local directory = dlg.data.file .. '/'
+      local extension = dlg.data.export_format
+      local directory = dlg.data.output_dir_path .. '/'
       local groupname = app.site.layer.name
 
-      if dlg.data.file then
+      if dlg.data.output_dir_path then
         for i, layer in ipairs(app.site.layer.layers) do
           -- Obtain the name for this file
           local layername = layer.name
@@ -71,13 +82,15 @@ function init(plugin)
           }
         end
 
-        plugin.preferences.export_dir = dlg.data.file
+        plugin.preferences.export_dir = dlg.data.output_dir_path
         plugin.preferences.export_filename_format = dlg.data.filename_format
-        plugin.preferences.export_format = dlg.data.format
+        plugin.preferences.export_format = dlg.data.export_format
         plugin.preferences.trim = dlg.data.trim
       end
     end,
+
     onenabled = function()
+      -- Only enable the command if the user has a group selected
       return app.site.layer.layers ~= nil
     end
   }
